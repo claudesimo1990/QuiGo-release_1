e62eba9439770b929a381b8d12bd9302ba8bcc1b
simo <jsimo1@yahoo.com>
2019-02-14 06:53:01 +0100

    landingpage

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -62,6 +62,7 @@
         </div>
       </div>
     </div>
+      <strong><h1 style="color:#89c53f;padding-top:50px;">Propositions de Voyages</h1></strong>
   </section>
 
   <section class="pricing py-5">
@@ -144,53 +145,85 @@
   <!-- Testimonials -->
   <section class="testimonials text-center bg-light">
     <div class="container">
-      <strong><h1 style="color:#89c53f;margin-bottom:50px;">Vos Avis comptent...</h1></strong>
+      <strong><h1 style="color:#89c53f;margin-bottom:60px;margin-top: -30px">Quelques Destinations preferées</h1></strong>
+      <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="mask flex-center">
+        <div class="container">
+          <div class="row align-items-center">
+            <div style="padding-top:12% !important;" class="col-md-12 col-12 order-md-1 order-2">
+              <h4>Allemagne<br>
+                Cameroun</h4>
+              <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
+                necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="mask flex-center">
+        <div class="container">
+          <div class="row align-items-center">
+            <div style="padding-top:12% !important;" class="col-md-12 col-12 order-md-1 order-2">
+              <h4>Belgique<br>
+                Cameroun</h4>
+              <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
+                necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="mask flex-center">
+        <div class="container">
+          <div class="row align-items-center">
+            <div style="padding-top:12% !important;" class="col-md-12 col-12 order-md-1 order-2">
+              <h4>Cameroun <br>
+                Allemagne</h4>
+              <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
+                necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
+             </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a> </div>
+<!--slide end--> 
+    </div>
+  </section>
+
+   <!-- Quelques Destinations preferer des vovageurs -->
+  <section class="testimonials text-center " style="background-color:#4d4e4a;">
+    <div class="container">
+      <strong><h1 style="color:#89c53f;margin-bottom:50px;">Vos avis comptent ...</h1></strong>
       <div class="row">
         <div class="col-lg-4">
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
             <img class="img-fluid rounded-circle mb-3" src="img/testimonials-1.jpg" alt="">
-            <h5>Guy Lancelot.</h5>
-            <p class="font-weight-light mb-0"><strong>"This is fantastic! Thanks so much guys!"</strong></p>
+            <h5 style="color:#fff">Guy Lancelot.</h5>
+            <p class="font-weight-light mb-0"><strong><span style="color:#89c53f">"Sincerement je ne saurais jamais comment vous remercier seul Dieu le fera"</span></strong></p>
           </div>
         </div>
         <div class="col-lg-4">
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
             <img class="img-fluid rounded-circle mb-3" src="img/testimonials-2.jpg" alt="">
-            <h5>Claude Simo.</h5>
-            <p class="font-weight-light mb-0"><strong>"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</strong></p>
+            <h5 style="color:#fff">Claude Simo.</h5>
+            <p class="font-weight-light mb-0"><strong><span style="color:#89c53f">"j ai recçue mon Paquet comme je l avais souhaiter je vous remercie continuer commsa!"</span></strong></p>
           </div>
         </div>
         <div class="col-lg-4">
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
             <img class="img-fluid rounded-circle mb-3" src="img/testimonials-3.jpg" alt="">
-            <h5>Hubert .</h5>
-            <p class="font-weight-light mb-0"><strong>"Thanks so much for making these free resources available to us!"</strong></p>
+            <h5 style="color:#fff">Hubert .</h5>
+            <p class="font-weight-light mb-0"><strong><span style="color:#89c53f">"Thanks so much for making these free resources available to us!"</span></strong></p>
           </div>
         </div>
       </div>
     </div>
   </section>
 
-  <!-- Call to Action -->
-  <section class="call-to-action text-white text-center">
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-xl-9 mx-auto">
-          <h2 class="mb-4">Ready to get started? Sign up now!</h2>
-        </div>
-        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
-          <form>
-            <div class="form-row">
-              <div class="col-12 col-md-9 mb-2 mb-md-0">
-                <input type="email" class="form-control form-control-lg" placeholder="Enter your email...">
-              </div>
-              <div class="col-12 col-md-3">
-                <button type="submit" class="btn btn-block btn-lg btn-primary">Rechercher</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </section>
+  
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.master')
 
 @section('css')
+<link href="https://raw.githubusercontent.com/daneden/animate.css/master/animate.css" rel="stylesheet">
 <link rel="stylesheet" href="/css/accueil.css">
 @endsection
 
@@ -8,5 +9,11 @@
 
 @include('accueil')
 
-
+@section('js')
+<script>
+$('#myCarousel').carousel({
+interval: 3000,
+})
+</script>
+@endsection
 @endsection
